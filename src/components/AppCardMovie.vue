04092bb607e8a6324ed12a707bce56312ae2508c
Mattia Giarrusso <giarrusso.m@yahoo.com>
2024-04-18 11:31:09 +0200

    Feat: fix card movie

--- src/components/AppCardMovie.vue
+++ src/components/AppCardMovie.vue
@@ -18,10 +18,15 @@
 <template>
 
     <div class="card my-2" style="width: calc(100% / 4);">
-        <img :src="store.movieImage + selectMovie.poster_path" :alt="selectMovie.title">
+        <div>
+            <img :src="store.movieImage + selectMovie.poster_path" :alt="selectMovie.title">
+        </div>
         <div class="card-body d-flex flex-column">
-            <h5 class="card-title "><strong>{{ selectMovie.title }}</strong></h5>
-            <span class="card-text"><strong>{{ selectMovie.original_title }}</strong></span>
+            <h5 class="card-title "><strong>Titolo: </strong>{{ selectMovie.title }}</h5>
+            <span class="card-text"><strong>Titolo originale: </strong>{{ selectMovie.original_title }}</span>
+            <span class="card-text"><strong>Lingua: </strong>{{ selectMovie.vote_average }}</span>
+            <span class="card-text"><strong>Lingua: </strong>{{ selectMovie.original_language }}</span>
+            <p class="card-text"><strong>Descrizione: </strong>{{ selectMovie.overview }}</p>
         </div>
     </div>
  
@@ -32,7 +37,7 @@
 @use '../style/generic';
 
 img {
-    border-radius: 10px;
+    border-radius: 5px;
 }
 
 </style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -19,8 +19,12 @@ export default {
 
 <template>
     <section>
-        <div class="container d-flex flex-wrap">
-            <AppCardMovie v-for="movie in store.moviesList" :key="movie.id" :selectMovie="movie"></AppCardMovie>
+        <div class="ms-container">
+
+            <div class="container d-flex flex-wrap ms-padding">
+                <AppCardMovie v-for="movie in store.moviesList" :key="movie.id" :selectMovie="movie"></AppCardMovie>
+            </div>
+
         </div>
     </section>
 </template>
@@ -31,6 +35,15 @@ export default {
 
 section {
     background-color:#2D262D;
+    
+    .ms-container {
+        height: 100%;
+    }
+
+    .ms-padding {
+        padding: 90px 0px;
+    }
+
 }
 
 
