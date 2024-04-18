e454065d69a21b2157209a31b592dcb14a3c4641
Mattia Giarrusso <giarrusso.m@yahoo.com>
2024-04-18 11:08:17 +0200

    Feat: fix the header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -19,17 +19,18 @@ export default {
 </script>
 
 <template>
-    <header class="d-flex align-items-center justify-content-spacebetween">
-        <div class="container-fluid d-flex">
-            <div class="logo">
-                <img :src="getImageUrl(logo)" alt="">
-            </div>
-        </div>
+    <header class="d-flex align-items-center justify-content-between px-4">
 
-        <div class="input-group d-flex align-items-center px-4">
+        <div class="logo">
+            <img :src="getImageUrl(logo)" alt="">
+        </div>
+        
+
+        <div class="input-group d-flex align-items-center ms-input-group">
             <input type="text" class="form-control" placeholder="Cerca un film o una serie TV..." v-model="store.searchContent" @keyup.enter="$emit('search')">
             <button class="btn ms-btn-search" type="button" @click="$emit('search')">Cerca</button>
         </div>
+
     </header>
 </template>
 
@@ -38,11 +39,12 @@ export default {
 @use '../style/generic';
 
 header {
+    width: 100%;
     height: 70px;
     background-color: black;
     color: red;
 
-    .logo {
+    .logo {        
         height: 60px;
 
         img {
@@ -50,12 +52,17 @@ header {
         }
     }
 
-    .ms-btn-search {
-        background-color: red;
-        &:hover {
-            background-color: darkred;
+    .ms-input-group {
+        width: 40%;
+
+        .ms-btn-search {
+            background-color: red;
+            &:hover {
+                background-color: darkred;
+            }
         }
     }
+
 }
 
 </style>
